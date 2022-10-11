a7e6935d401dec7d5c722444a6bb790c139a5539
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2022-10-11 11:29:40 +0200

    Added demo - beginning

---
+++ Beginning/src/js/main.js
@@ -0,0 +1,26 @@
+// alert("Hello world!");
+
+const firstname = "Sebastian";
+let age = 43;
+let isMarried = true;
+
+console.log("Hej" + " " + firstname);
+// console.log(firstname);
+
+age = age + 1;
+age += 1;
+age++;
+
+console.log(age);
+
+age--;
+age -= 1;
+age = age - 1;
+
+console.log(age);
+
+console.log("Modulus:", age % 2);
+
+age++;
+
+console.log("Modulus:", age % 7);
